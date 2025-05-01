6ed6ffd2bcb2a5e1200bdbe1ce2cf137ac4b6c69
LeshiyAda9H <114980114+LeshiyAda9H@users.noreply.github.com>
2025-05-01 21:00:06 +0300

    Add: BarChart

--- client/src/components/CreditResult.tsx
+++ client/src/components/CreditResult.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { Typography } from '@mui/material';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 const CreditResult = () => {
   const output = useSelector((state: RootState) => state.credit.output);
@@ -8,6 +9,14 @@ const CreditResult = () => {
   if (!output) {
     return <Typography>Введите данные и нажмите "Рассчитать"</Typography>;
   }
+
+  //Данные для графика
+  const data = [
+    {
+      name: 'Вероятность одобрения',
+      probability: output.approvalProbability,
+    }
+  ]
 
   return (
     <div>
@@ -18,6 +27,21 @@ const CreditResult = () => {
       <Typography>
         Рекомендуемая сумма: {output.recommendedAmount} руб.
       </Typography>
+
+      <div style = {{marginTop: '20px'}}>
+        <Typography variant='subtitle1'>
+          График вероятности:
+        </Typography>
+        <ResponsiveContainer width='100%' height={200}>
+          <BarChart data = {data}>
+            <CartesianGrid strokeDasharray='3 3'/>
+            <XAxis dataKey='name'/>
+            <YAxis domain={[0,100]}/>
+            <Tooltip/>
+            <Bar dataKey='probability' fill='#8884d8'/>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
